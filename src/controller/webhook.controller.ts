842f6df2c0d018b65c0beb3635a17cc25f81287f
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-07-18 16:20:20 +0530

    fix: added webhook func

--- src/controller/webhook.controller.ts
+++ src/controller/webhook.controller.ts
@@ -1,3 +1,51 @@
-export const createWebhookRouter = () => {};
+import axios from 'axios';
+import { Response, Request } from 'express';
+export const createWebhookRouter = (req: Request, res: Response) => {
+  try {
+    const { webhookUrl, addresss, programIds } = req.body;
+    const response = axios.post(
+      `https://api.helius.xyz/v0/webhooks`,
+      {
+        webhookURL: webhookUrl,
+        transactionTypes: ['Any'],
+        accountAddresses: addresss || [],
+        accountAddressOwners: programIds || [],
+        encoding: 'jsonParsed',
+        webhookType: 'raw',
+        txnStatus: 'all',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('Webhook created', response);
+  } catch (error) {
+    console.log('Error while creating webhook', error);
+  }
+};
 
-export const updateWebhookRouter = () => {};
+export const updateWebhookRouter = (req: Request, res: Response) => {
+  try {
+    const { webhookID, addresss, webhookUrl } = req.body;
+    const response = axios.post(
+      `https://api.helius.xyz/v0/webhooks/${webhookID}`,
+      {
+        transactionTypes: ['Any'],
+        accountAddresses: addresss || [],
+        encoding: 'jsonParsed',
+        webhookType: 'raw',
+        txnStatus: 'all',
+        webhookURL: webhookUrl,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
